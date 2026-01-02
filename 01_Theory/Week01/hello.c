4240ac916cb45a443df4b04c4beddf838aa3ca82
Denis Lom <lom.denis@seznam.cz>
2026-01-02 17:00:54 +0100

    hello.c

---
+++ 01_Theory/Week01/hello.c
@@ -0,0 +1,10 @@
+ //cc hello.c -o hello -lcs50
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string name = get_string("What's your name? ");
+    printf("hello, %s\n", name);
+    return (0);
+}
