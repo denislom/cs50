28036fb41bf44ea4f99cb39761dcfc271aca46aa
Denis Lom <lom.denis@seznam.cz>
2026-02-10 21:50:46 +0100

    function is_tie()

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -191,12 +191,18 @@ int find_min(void)
 bool is_tie(int min)
 {
     // TODO
-    return false;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].eliminated == false && candidates[i].votes != min)
+            return false;
+    }
+    return true;
 }
 
 // Eliminate the candidate (or candidates) in last place
 void eliminate(int min)
 {
     // TODO
+    
     return;
 }
